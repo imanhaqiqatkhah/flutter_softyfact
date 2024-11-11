5ff86242b57b4bf3c13727e03759cedfd31eb0e5
imanhaqiqatkhah <imanhaqiqatkhah@gmail.com>
2024-11-11 19:08:36 +0330

    end nodemon install

--- server/index.js
+++ server/index.js
@@ -1,2 +1,12 @@
 console.log("Hello, World")
-// print('Hello, World')
+// print('Hello, World');
+
+const express = require("express")
+
+const PORT = 3000
+
+const app = express()
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Connected at port ${PORT} `)
+})
